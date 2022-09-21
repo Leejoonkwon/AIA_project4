093699ab703709f41398401fd3d22c8fd6cd5fe1
Leejoonkwon <107663853+Leejoonkwon@users.noreply.github.com>
2022-09-21 11:56:06 +0900

    0921

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ transform = transforms.Compose([
         transforms.ToTensor(), # 데이터를 PyTorch의 Tensor 형식으로 바꾼다.
         transforms.Normalize(mean=(0.5,), std=(0.5,)) # 픽셀값 0 ~ 1 -> -1 ~ 1
 ])
-                     
+                        
 # MNIST 데이터셋을 불러온다. 지정한 폴더에 없을 경우 자동으로 다운로드한다.
 mnist = datasets.MNIST(root='data', download=True, transform=transform)
                        
