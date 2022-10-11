2458ad6c702153e7249753cb63964fb4fb4765ea
Leejoonkwon <qnfk7895@naver.com>
2022-10-11 16:51:29 +0900

    20221011

--- crawling(crop추가).py
+++ crawling(crop추가).py
@@ -66,3 +66,52 @@ for image in images:
 
 driver.close()
 
+import cv2
+import numpy as np
+import os
+
+path_dir = "이정재/"
+file_list = os.listdir(path_dir)
+
+file_list[0]
+len(file_list)
+print(len(file_list))
+
+file_name_list = []
+
+for i in range(len(file_list)):
+    file_name_list.append(file_list[i].replace(".jpg",""))
+print(file_name_list)
+
+
+print(file_name_list[0])
+def Cutting_face_save(image, name):
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    eye_casecade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        # cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0),2)
+        cropped = img[y - int(h/4):y + h + int(h/4), x - int(w/4):x + w + int(w/4)]
+        # roi_gray = gray[y:y+h, x:x+w]
+        # roi_color = img[y:y+h, x:x+w]
+        # eyes = eye_casecade.detectMultiScale(roi_gray)    
+        # for (ex, ey, ew, eh) in eyes:
+        #     cv2.rectangle(roi_color, (ex,ey), (ex+ew, ey+eh),(0,255,0),2)
+    # for (x,y,w,h) in faces:
+    #     cv2.rectangle(image, (x,y), (x+w, y+h), (255,0,0), 2)
+    #     cropped = image[y: y+h, x: x+w]
+    #     resize = cv2.resize(cropped, (250,250))
+    #     # cv2.imshow("crop&resize", resize)
+    #     cv2.waitKey(0)
+    #     cv2.destroyAllWindows()
+
+        # 이미지 저장하기
+        cv2.imwrite(f"이정재/{name}.jpg", cropped)
+        
+for name in file_name_list:
+    img = cv2.imread("이정재/"+name+".jpg")
+    Cutting_face_save(img, name)
+
+
+
--- test12.py
+++ test12.py
@@ -1,4 +1,32 @@
-print(2/3)
-print(2//3)
-print(4/6)
-print(4//3)
+import cv2
+import numpy as np
+face_cascade = cv2.CascadeClassifier('C:\\team_project\AIA_project4/haarcascade_frontalface_default.xml')
+eye_casecade = cv2.CascadeClassifier('C:\\team_project\AIA_project4/haarcascade_eye.xml')
+
+ff = np.fromfile('C:\\team_project\AIA_project4\\정우성.jpg',np.uint8)
+# img = cv2.imread('C:\\team_project\AIA_project4\\test12.jpg')
+img = cv2.imdecode(ff,cv2.IMREAD_UNCHANGED)
+print(cv2.__version__)
+print(img.shape)
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+faces = face_cascade.detectMultiScale(gray, 1.3,5)
+imgNum  = 0
+for (x,y,w,h) in faces:
+    cropped = img[y - int(h / 4):y + h + int(h / 4), x - int(w / 4):x + w + int(w / 4)]
+    # 이미지를 저장
+    cv2.imwrite("thumbnail" + str(imgNum) + ".png", cropped)
+    imgNum += 1
+
+    # cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0),2)
+    # roi_gray = gray[y:y+h, x:x+w]
+    # roi_color = img[y:y+h, x:x+w]
+    # eyes = eye_casecade.detectMultiScale(roi_gray)
+    # for (ex, ey, ew, eh) in eyes:
+    #     cv2.rectangle(roi_color, (ex,ey), (ex+ew, ey+eh),(0,255,0),2)
+
+# cv2.imshow('Image view', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
